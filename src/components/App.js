503c413392e07c99050613539fccbe634f696707
Edmund <edmundgachanja@gmail.com>
2025-01-12 14:24:50 +0300

    Finishes the lab

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,26 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+        name = {user.name}
+        city = {user.city}
+        color = {user.color}
+      />
+      <About 
+          links = {user.links}
+          bio = {user.bio}
+      />
     </div>
   );
 }
-
+// const user = {
+//   name: "Liza",
+//   city: "New York",
+//   bio: "I made this!",
+//   color: "firebrick",
+//   links: {
+//     github: "https://github.com/liza",
+//     linkedin: "https://www.linkedin.com/in/liza/",
+//   },
+// };
 export default App;
